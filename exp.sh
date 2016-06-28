f994665821246ccb3370d4beaadf721f7956a7f5
Toshio Endo <endo@is.titech.ac.jp>
2016-06-28 23:25:31 +0900

    editing

--- exp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# temp command by Endo
-make clean
-dir=$HOME/export/hhrt
-#scp -r . 192.168.8.59:$dir
-rm -rf $dir
-cp -r -p . $dir
-date > $dir/date.txt
-echo Written directory $dir
--- up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# temp command by Endo
-make clean
-dir=backup/hhrt-`date +%y%m%d-%H%M`
-#scp -r . 192.168.8.59:$dir
-cp -r -p . $HOME/$dir
-echo Written directory $dir
